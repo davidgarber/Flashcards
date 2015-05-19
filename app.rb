9993ed13af125a2882c5feb536c9d447ceb5697a
Cory Ribson & David Garber & David Abel & steven francisco <student@epicodus.com>
2015-05-19 15:17:34 -0700

    adds working links to start studying and neato css styling for popups

--- app.rb
+++ app.rb
@@ -5,3 +5,10 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/flashcard') do
+  max_num=Flashcard.count
+  rand_num = Random.new.rand(1...max_num)
+  @flashcard = Flashcard.find(rand_num)
+  erb(:flashcard)
+end
