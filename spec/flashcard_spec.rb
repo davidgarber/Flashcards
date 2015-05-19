b69a75cd2f94994aec6cf3a8cf8b8f667cb5090f
Cory Ribson & David Garber & David Abel & steven francisco <student@epicodus.com>
2015-05-19 11:03:29 -0700

    checks to make sure correct is set to false.

--- spec/flashcard_spec.rb
+++ spec/flashcard_spec.rb
@@ -1,9 +1,14 @@
 require('spec_helper')
 
 describe(Flashcard) do
-
-    it("validates the presence of a name") do
-      flashcard = Flashcard.new({:name => ""})
-      expect(flashcard.save()).to(eq(false))
-    end
+  it("validates the presence of a name") do
+    flashcard = Flashcard.new({:name => ""})
+    expect(flashcard.save()).to(eq(false))
   end
+  it("checks to make sure correct is set to false") do
+    flashcard= Flashcard.new(name: "Test")
+    flashcard.save
+    expect(flashcard.correct).to(eq(false))
+  end
+
+end
