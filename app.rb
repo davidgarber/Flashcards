252d011d57eb20a2e0deaca46b766de0816a763e
Guest User <student@epicodus.com>
2015-05-21 14:04:55 -0700

    Adds answer response.

--- app.rb
+++ app.rb
@@ -29,18 +29,34 @@ get('/correct') do
   erb(:correct_cards)
 end
 
-
 get('/quiz') do
   @flashcard = Flashcard.get_question
   @wrong1 = @flashcard.wrong1
   @wrong2 = @flashcard.wrong2
   @wrong3 = @flashcard.wrong3
 
-  @flashcards = Flashcard.all()
-  # #@venues_checked = @band.venues.ids
   @wronganswer1 = Flashcard.find(@wrong1)
   @wronganswer2 = Flashcard.find(@wrong2)
   @wronganswer3 = Flashcard.find(@wrong3)
 
   erb(:quiz)
+end
+
+post('/quiz') do
+  @answer = params.fetch("answer")
+  @definition = params.fetch("definition")
+  erb(:success)
+  #redirect('/quiz')
+end
+
+# patch('/quiz') do
+#   @answer = params.fetch("answer")
+#   @flashcard = Flashcard.find(params.fetch("id"))
+#   #erb(:quiz)
+#
+#   redirect('/quiz')
+# end
+
+get('/success') do
+  erb(:success)
 end
